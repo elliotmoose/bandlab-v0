72f6ce2cd9cf35391a940da6ae0d67aa364490f4
Elliot Koh <kyzelliot@gmail.com>
2023-04-08 19:19:04 +0800

    add navbar styling

--- public/src/components/nav-bar/nav-bar.js
+++ public/src/components/nav-bar/nav-bar.js
@@ -4,12 +4,41 @@ class NavBarComponent extends HTMLElement {
   }
 
   connectedCallback() {
-    this.innerHTML = `
-      <ul class="nav-bar">
-        <li><a href="/posts">Posts</a></li>
-        <li><a href="/player">Player</a></li>    
-      </ul>
-    `;
+    // get current window path i.e. /posts or /player
+    const path = window.location.pathname;
+
+    const ul = document.createElement("ul");
+    ul.setAttribute("class", "nav-bar");
+
+    const postsLink = document.createElement("a");
+    postsLink.setAttribute("href", "/posts");
+    postsLink.setAttribute(
+      "style",
+      `text-decoration: none; color: ${
+        path === "/posts" ? "black" : "#ccc"
+      }; font-weight: bold;`
+    );
+    postsLink.textContent = "Posts";
+
+    const playerLink = document.createElement("a");
+    playerLink.setAttribute("href", "/player");
+    playerLink.setAttribute(
+      "style",
+      `text-decoration: none; color: ${
+        path === "/player" ? "black" : "#ccc"
+      }; font-weight: bold;`
+    );
+    playerLink.textContent = "Player";
+
+    const postsLi = document.createElement("li");
+    postsLi.appendChild(postsLink);
+    ul.appendChild(postsLi);
+
+    const playerLi = document.createElement("li");
+    playerLi.appendChild(playerLink);
+    ul.appendChild(playerLi);
+
+    this.appendChild(ul);
   }
 }
 
